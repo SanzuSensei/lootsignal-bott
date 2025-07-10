4dd699fadd99346b142236f34412fa8bdd82807d
Sanzu <your_email@example.com>
2025-07-10 12:17:31 -0400

    new changes

--- dashboard/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    CLIENT_ID: '1392196089142313060',
-    CLIENT_SECRET: 'wu0u5X0qCplH8uz82jn_ZOpQ56N9TyN_',
-    CALLBACK_URL: 'http://localhost:3000/auth/callback',
-    SCOPES: ['identify', 'guilds'],
-    SESSION_SECRET: '0930'
-};
-
